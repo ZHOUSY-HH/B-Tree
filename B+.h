52a9e94b652ff4f5340bea964770f27b02474159
GreeenieQWQ <55385374+GreenieQwQ@users.noreply.github.com>
2019-11-03 12:12:09 +0800

    Add files via upload

---
+++ B+.h
@@ -0,0 +1,23 @@
+#ifndef BPLUS_H
+#define BPLUS_H
+
+template<typename Record, int order>
+class B_Tree
+{
+private:
+struct Node
+{
+    
+
+    Node();
+};
+
+public:
+
+
+private:
+    
+};
+
+
+#endif
