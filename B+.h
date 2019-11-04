fa21e82fe4228df7f31c096fd5cdb8fe6b4905bf
GreeenieQWQ <55385374+GreenieQwQ@users.noreply.github.com>
2019-11-04 22:00:24 +0800

    Modifying B+.h

--- B+.h
+++ B+.h
@@ -1,22 +1,33 @@
 #ifndef BPLUS_H
 #define BPLUS_H
 
-template<typename Record, int order>
+template<typename data_type, typename key_type, int order, int L>
 class B_Tree
 {
 private:
-struct Node
-{
-    
-
-    Node();
-};
+    struct Node
+    {
+        int count;
+        enum NodeTag { LEAF, NODE };
+        union branch
+        {
+            Node* node[order];
+            Leaf* leaf[order];
+        };
+        key_type key[order-1];
+        NodeTag tag;
+        Node();
+    };
+    struct Leaf
+    {
+        data_type data[L];
+    };
 
 public:
-
+    
 
 private:
-    
+    Node* root;
 };
 
 
